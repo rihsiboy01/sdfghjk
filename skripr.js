1eca40f607e629925a63146bdadc3b82243b4844
rihsiboy01 <auzcom78@gmail.com>
2024-10-25 15:21:21 +0300

    Add files via upload

---
+++ skripr.js
@@ -0,0 +1,25 @@
+var a = alert('salom ')
+var l = prompt('qanday yordam bera olaman')
+var s = alert('hali munday ishlar qila olmayman')
+var j = prompt('yana qanday yordam bera olaman')
+var d = alert('hali hechqnday ish qila olmayman')
+const h = prompt('nima yordam')
+var d = alert('iltimos sokinmang')
+var c = prompt('hizmat sizni qoniq tirdimi?')
+
+
+
+
+
+console.log( typeof a);
+console.log( typeof l);
+console.log( typeof s);
+console.log( typeof j);
+console.log( typeof d);
+console.log( typeof c);
+console.log( typeof h);
+
+
+
+
+
